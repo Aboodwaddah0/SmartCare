7b8ec69f3c0ba90a10aecd33bb778bf4a4007ddd
T-Abdulqader.Mohamma <aboodwaddah516@gmail.com>
2026-04-26 16:17:33 +0300

    fix error in PrescriptionRepository

--- SmartCare/src/main/java/com/example/SmartCare/repository/PrescriptionRepository.java
+++ SmartCare/src/main/java/com/example/SmartCare/repository/PrescriptionRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 public interface PrescriptionRepository extends MongoRepository<Prescription, String> {
 
     List<Prescription> findByPatientId(Long patientId);
-
-    List<Prescription> findByDoctorId(Long doctorId);
 }
